32a12e85173af510202fe650981f02f78dfd6cf4
Spocin <7z\kt\h.TS:87izb5\*dM'|7h>
2022-04-15 00:14:53 +0200

    Modified files to Use TypeScript

--- app.ts
+++ app.ts
@@ -1,19 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+/*Imports*/
+import express, {Request,Response} from "express";
+import httpError, {HttpError} from "http-errors";
+import {NextFunction} from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -23,12 +25,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req: Request, res:Response, next:NextFunction) => {
+  next(httpError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err:HttpError, req:Request, res:Response, next:NextFunction) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,4 +40,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+export {app}
--- routes/users.ts
+++ routes/users.ts
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+/*Imports*/
+import express from "express";
+
+/*Declarations*/
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-module.exports = router;
+export {router};
