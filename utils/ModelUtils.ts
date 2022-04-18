d36d0d21ec50bae2b99623a088769fc44193057a
Spocin <7z\kt\h.TS:87izb5\*dM'|7h>
2022-04-18 22:23:18 +0200

    Added function to generate msg for capital letter validation.

--- utils/ModelUtils.ts
+++ utils/ModelUtils.ts
@@ -5,3 +5,7 @@ export function generateRequiredMsg() {
 export function generateLengthMsg(from: number, to: number) {
     return `Must be between ${from} and ${to} characters`;
 }
+
+export function generateCapitalLetterMsg () {
+    return "Has to start with capital letter"
+}
