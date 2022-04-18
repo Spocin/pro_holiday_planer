244007e7b396749254a0aeb9a4032294a72ee219
Spocin <7z\kt\h.TS:87izb5\*dM'|7h>
2022-04-18 21:24:16 +0200

    Added User model to be synced on db init.

--- config/sequelize/initialize.ts
+++ config/sequelize/initialize.ts
@@ -1,3 +1,16 @@
 import {sequelizeDbConnection} from "./sequelizeDbConnection";
 
-//TODO create models
+import User from "../../models/User";
+
+
+const dbInit = async () => {
+
+    await User.sync({
+        force: true
+    });
+    console.log("Synced User");
+}
+
+export {dbInit}
+
+//TODO create rest of the models
