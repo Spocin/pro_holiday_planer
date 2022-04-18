d35e9e1b07a5d330f06024223b4a62ff23e61791
Spocin <7z\kt\h.TS:87izb5\*dM'|7h>
2022-04-19 00:19:49 +0200

    Added unique constraint to Country

--- models/Country.ts
+++ models/Country.ts
@@ -35,6 +35,7 @@ Country.init({
     name: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
         validate: {
             notEmpty: {
                 msg: generateRequiredMsg()
