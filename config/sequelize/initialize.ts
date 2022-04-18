3cc32b0e49a36168f405cd697f5bbd88215ddd75
Spocin <7z\kt\h.TS:87izb5\*dM'|7h>
2022-04-19 00:59:28 +0200

    Added User and Suitcase to dbInit

--- config/sequelize/initialize.ts
+++ config/sequelize/initialize.ts
@@ -1,27 +1,54 @@
 import City from "../../models/City";
 import Country from "../../models/Country";
-import {sequelizeDbConnection} from "./sequelizeDbConnection";
+import User from "../../models/User";
+import Suitcase from "../../models/Suitcase";
+
+import { sequelizeDbConnection } from "./sequelizeDbConnection";
+
 
 const dbInit = async () => {
 
-    /*CONSTRAINTS*/
     Country.hasMany(City, {
-        as: 'countries',
+        as: "countries",
         foreignKey: "fk_country",
         constraints: true,
-        onDelete: 'CASCADE'
+        onDelete: "CASCADE"
     });
 
     City.belongsTo(Country, {
-        as: 'country',
-        foreignKey: 'id'
+        as: "country",
+        foreignKey: "fk_country"
     });
+
+    User.hasMany(Suitcase, {
+        as: "suitcases",
+        foreignKey: "fk_user",
+        constraints: true,
+        onDelete: "CASCADE"
+    });
+
+    Suitcase.belongsTo(User, {
+        as: "suitcase_user",
+        foreignKey: "fk_user"
+    });
+
+    City.hasMany(Suitcase, {
+        as: "suitcases",
+        foreignKey: "fk_city",
+        constraints: true,
+        onDelete: "CASCADE"
+    });
+
+    Suitcase.belongsTo(City, {
+        as: "suitcase_city",
+        foreignKey: "fk_city"
+    });
+
+    //TODO create rest of constraints and models
 
     await sequelizeDbConnection.sync({
         force: true
-    })
-
-    //TODO create rest of the models
+    });
 }
 
 export {dbInit}
