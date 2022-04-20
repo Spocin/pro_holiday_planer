8984ab2ecca443e5bcf91bce87a5a5116cf4b433
Spocin <7z\kt\h.TS:87izb5\*dM'|7h>
2022-04-20 12:15:00 +0200

    Implemented password hashing utils

---
+++ auth/authUtils.ts
@@ -0,0 +1,10 @@
+import bcrypt from "bcryptjs";
+
+export const hashPassword = async (plainPassword: string) => {
+    const salt = await bcrypt.genSalt();
+    return await bcrypt.hash(plainPassword,salt);
+}
+
+export const comparePassword = async (plainPassword: string, hash: string) => {
+    return await bcrypt.compare(plainPassword,hash);
+}
